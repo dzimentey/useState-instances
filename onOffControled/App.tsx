28368fe34d36734323488cf1d739a679b78f56f7
dzimentey <60585408+dzimentey@users.noreply.github.com>
2021-08-11 20:29:30 +0300

    Main component App.tsx

--- onOffControled/App.tsx
+++ onOffControled/App.tsx
@@ -9,13 +9,7 @@ function App() {
             <UncontrolledRating />
             <hr/>
             <OnOffControlled on={switcher} onChange={setSwitcher}/>
-            {/*<Accordion titleValue={'first menu'} collapsed={true}/>*/}
-            {/*<Rating value={0}/>*/}
-            {/*<Rating value={1}/>*/}
-            {/*<Rating value={3}/>*/}
-            {/*<Rating value={4}/>*/}
-            {/*<Rating value={5}/>*/}
-            {/*<Accordion titleValue={'second menu'} collapsed={false}/>*/}
+            
         </div>
     );
 }
