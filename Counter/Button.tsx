fb7594e13ddf785935ac8b9500f0e84b78095f9c
dzimentey <60585408+dzimentey@users.noreply.github.com>
2021-08-18 20:47:55 +0300

    add className={s.button}

--- Counter/Button.tsx
+++ Counter/Button.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import s from './Counter.module.css'
 type ButtonPropsType = {
     callBack:()=> void
     title: string
@@ -7,7 +8,7 @@ type ButtonPropsType = {
 export const Button = (props: ButtonPropsType) => {
     return(
 
-          <button onClick={props.callBack} disabled={props.count ? props.count >= 5 : false}>{props.title}</button>
+          <button className={s.button} onClick={props.callBack} disabled={props.count ? props.count >= 5 : false}>{props.title}</button>
 
     )
 }
